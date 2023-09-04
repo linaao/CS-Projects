ebdaafd73fab802c73083f841ed627747029f8c2
Lina Ihssane <cheekylin95@gmail.com>
2023-09-04 11:07:42 +0100

    check connection in php

---
+++ Web Page1:2/welcome.php
@@ -0,0 +1,18 @@
+<?php
+// creat connection
+$admin = "root";
+$nlocal = "localhost";
+$pasword = "";
+$connect = new mysqli($nlocal, $admin, $pasword);
+
+// check connection 
+
+if ( $connect -> connect_error) {
+    die ("connection failed" . $connect -> connect_error);
+}
+echo "connection success <br>";
+
+
+
+
+?>
